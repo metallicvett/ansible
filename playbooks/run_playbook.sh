f0784fd0ec1d3855a968ffb49a8b656f42db4570
Jerry <jgmbjunk@gmail.com>
2025-11-16 12:36:55 -0500

    save the ansible-navigation documents

--- playbooks/run_playbook.sh
+++ playbooks/run_playbook.sh
@@ -1,1 +1,4 @@
+# https://docs.ansible.com/projects/ansible/latest/getting_started_ee/run_execution_environment.html#running-custom-execution-environment
+
+
 ansible-navigator run playbooks/playbook.yml --execution-environment-image windows_ee --mode stdout --pull-policy missing --container-options='--user=0'
